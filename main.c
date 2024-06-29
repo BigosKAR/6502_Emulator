b8c2447ba2653a1c1988cf86866d44b861c4fb02
Oskar K <oskarkloczko13@gmail.com>
2024-06-29 12:23:20 +0200

    added stack + stack math operations

--- main.c
+++ main.c
@@ -1,27 +1,55 @@
 #include <stdio.h>
+#define STACK_MAX 256
+
 
 struct {
     unsigned char *ip; // Pointer pointing to the instructions
     unsigned long long register_A; // Register for storing values
+    unsigned long long stack[STACK_MAX];
+    unsigned long long *sp; // stack top pointer
 }vm;
 
 typedef enum{
-    OP_INC, // increase register value by 1
-    OP_DEC, // decrease register value by 1
-    OP_ADDI, // add immediate value to register
-    OP_SUBI, // subtract immediate value from register
+    OP_PUSHI, // push immediate value to stack
+    OP_ADD, // add two top values from stack
+    OP_SUB, // subtract two top values from stack
+    OP_DIV, // divide two top values from stack
+    OP_MUL, // multiply two top values from stack
+    OP_POP_RES, // pop the result from stack
+
+    //REGISTER OPERATIONS
+    //OP_INC, // increase register value by 1
+    //OP_DEC, // decrease register value by 1
+    //OP_ADDI, // add immediate value to register
+    //OP_SUBI, // subtract immediate value from register
+    
     OP_DONE // terminate program
 }opcode;
 
 typedef enum result{
-    SUCCESS,
-    ERROR,
+    SUCCESS, // will have value 0
+    ERROR_OPCODE_NOT_FOUND, // will have value 1
+    ERROR_DIVISION_BY_ZERO, // will have value 2
 }result;
 
 void reset(void)
 {
     puts("Resetting the VM");
     vm = (typeof(vm)) {NULL};
+    vm.sp= vm.stack;
+
+}
+
+void stack_push(unsigned long long value)
+{
+    *vm.sp = value;
+    vm.sp++;
+}
+
+unsigned long long stack_pop(void)
+{
+    vm.sp--;
+    return *vm.sp;
 }
 
 result vm_interpret(unsigned char *bytecode){
@@ -33,7 +61,55 @@ result vm_interpret(unsigned char *bytecode){
         unsigned char instruction = *vm.ip++;
         switch(instruction)
         {
-            case OP_INC:{
+            case OP_PUSHI: {
+                // push immediate value to stack
+                unsigned char arg = *vm.ip++;
+                stack_push(arg);
+                break;
+            }
+            case OP_ADD: {
+                // pop two values from stack, add, then push the result
+                unsigned long long a = stack_pop();
+                unsigned long long b = stack_pop();
+                unsigned long long c = a + b;
+                stack_push(c);
+                break;
+            }
+            case OP_SUB: {
+                // pop two values from stack, subtract, then push the result
+                unsigned long long a = stack_pop();
+                unsigned long long b = stack_pop();
+                unsigned long long c = a - b;
+                stack_push(c);
+                break;
+            }
+            case OP_DIV: {
+                //pop two values from stack, divide, and then push the result
+                unsigned long long a = stack_pop();
+                if (a == 0)
+                {
+                    return ERROR_DIVISION_BY_ZERO;
+                }
+                unsigned long long b = stack_pop();
+                unsigned long long c = b/a;
+                stack_push(c);
+                break;
+            }
+            case OP_MUL: {
+                // pop two values from stack, multiply, then push the result
+                unsigned long long a = stack_pop();
+                unsigned long long b = stack_pop();
+                unsigned long long c = a * b;
+                stack_push(c);
+                break;
+            }
+            case OP_POP_RES: {
+                // pop result from stack, move it to register A
+                unsigned long long result = stack_pop();
+                vm.register_A = result;
+                break;
+            }
+            /*case OP_INC:{
                 vm.register_A++;
                 break;
             }
@@ -53,11 +129,12 @@ result vm_interpret(unsigned char *bytecode){
             vm.register_A -= arg;
             break;
             }
+            */
             case OP_DONE: {
                 return SUCCESS;
             }
             default:
-                return ERROR;
+                return ERROR_OPCODE_NOT_FOUND;
         }
     }
     return SUCCESS;
